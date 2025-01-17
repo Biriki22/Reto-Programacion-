a4639b054ef7934378d91418fd9d5fd2abf9bffc
Biriki22 <markel.neila.2004@gmail.com>
2025-01-17 22:45:44 +0100

    reto

---
+++ reto.py
@@ -0,0 +1,45 @@
+def ej1():
+    """
+    Para conocer los gustos favoritos de los usuarios se ha seleccionado al azar un grupo de
+    100 personas a las cuales se les ha pedido que introduzcan en el sistema su género favorito
+    (Teoría política, narrativa fantástica, terror espacial o drama coreano). Cada usuario podrá
+    elegir, mediante un número, entre cada uno de estos géneros y al acabar (cuando las 100
+    personas han seleccionado género) el programa deberá indicar qué opción ha sido la más
+    seleccionada y los votos que ha recibido cada uno de ellos ordenados de mayor a menor.
+    """
+   
+def ej2():
+    """
+    El segundo programa nos va a servir para poder validar si un usuario se ha pasado con la
+    fecha de entrega del libro o no. Teniendo en cuenta la fecha actual (con el método explicado
+    en el PPTX) se deberá comprobar, teniendo en cuenta cuantos días como máximo se
+    pueden reservar los libros, si hay que sancionar al usuario o no. En cualquiera de los casos
+    hay que mostrar por pantalla la decisión a tomar con los mensajes "Se ha pasado de fecha"
+    o "La fecha está dentro de plazo".
+    """
+
+def ej3():
+    """
+    Nos han pedido hacer un algoritmo para introducir todos los libros en el sistema, para ello
+    vamos a pedir al usuario que vaya metiendo primero el título del libro y luego el número de
+    páginas. Como no sabemos cuántos libros van a meter el sistema dejará de pedir números
+    una vez el usuario meta la palabra “Terminé”. Lo que pide el sistema es lo siguiente:
+    - La media de páginas de todos los libros.
+    - El título del libro con más páginas.
+    - El título del libro con menos páginas.
+    - La cantidad de libros que tienen 300 páginas o más.
+    """
+
+
+def ej4():
+    """
+    - El siguiente algoritmo a desarrollar, una vez sabido que la cantidad de libros es de 45000
+    es uno que compruebe si la cantidad de libros actual es la total o si faltan libros por
+    devolver. Para ello, el usuario, el día que le toque hacer inventario, escribirá manualmente el
+    título de todos los libros y el programa parará de pedir títulos de libros una vez meta un
+    título vacío (“”). Si la cantidad de libros metidos es igual a la cantidad que hay en la
+    biblioteca (45000 libros) podrá un mensaje diciendo que el inventario es correcto, si no tiene
+    que indicar cuantos libros faltan por devolver.
+    """
+
+    
